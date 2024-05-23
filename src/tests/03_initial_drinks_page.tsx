70a0363049ab2de7fe97943c9cd6f5a24d860ac2
valedobrandi <valedobrandi@gmail.com>
2024-05-23 07:16:29 -0300

    teste_03

---
+++ src/tests/03_initial_drinks_page.tsx
@@ -0,0 +1,47 @@
+import App from "../App"
+import Wrapper from "./helpers/context_Wrapper"
+import renderWithRouter from "./helpers/render_with_router"
+import { screen } from '@testing-library/react'
+import { vi } from 'vitest';
+import fetch_api from "./mock/fetch";
+import userEvent from "@testing-library/user-event";
+
+
+
+describe('Route "/drinks"', () => {
+  vi.spyOn(global, 'fetch').mockImplementation(fetch_api);
+  it('Render all recipes cards', async () => {
+    renderWithRouter(
+      <Wrapper>
+        <App />
+      </Wrapper>,
+      { route: '/drinks' },
+    );
+
+    expect(await screen.findAllByRole('heading', {level: 6})).toHaveLength(2);
+    expect(screen.getByRole('heading', { name: /GG/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', {  name: /A1/i})).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: /GG/i })).toBeInTheDocument();
+    expect(screen.getByRole('img', {  name: /A1/i})).toBeInTheDocument();
+  });
+
+  it('"Select By Category" return the correct recipes', async () => {
+    renderWithRouter(
+      <Wrapper>
+        <App />
+      </Wrapper>,
+      { route: '/meals' },
+    );
+
+    expect(await screen.findAllByRole('heading', {level: 6})).toHaveLength(2);
+    await userEvent.click(screen.getByRole('combobox'));
+    await userEvent.click(screen.getByRole('option', {  name: /breakfast/i}));
+    expect(await screen.findByRole('heading', { name: /breakfast 1/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', {  name: /breakfast 2/i})).toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole('combobox'));
+    await userEvent.click(screen.getByRole('option', {  name: /all/i}));
+    expect(await screen.findByRole('heading', { name: /corba/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', {  name: /Kumpir/i})).toBeInTheDocument();
+  });
+});
